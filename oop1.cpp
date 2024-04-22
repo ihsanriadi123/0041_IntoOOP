5d4aed80a95e4482a6fc28ed006bbfb0897a96b5
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-04-22 15:01:27 +0700

    variable dari class

--- oop1.cpp
+++ oop1.cpp
@@ -14,3 +14,26 @@ class mahasiswa {
 
     }
 };
+
+class matakuliah {
+    
+    private :
+        string kodeMK;
+        string namaMK;
+        int sks;
+    
+    public :
+        void inputMK () {
+            cout << "kode MK: ";
+            cin >> kodeMK;
+            cout << "nama MK: ";
+            cin >> namaMK;
+            cout << "Sks: ";
+            cin >> sks;
+        }
+        void output () {
+            cout << "kode MK\t: " << kodeMK <<endl;
+            cout << "nama MK\t: " << namaMK <<endl;
+            cout << "SKS \t: " << sks <<endl;
+        }
+};
